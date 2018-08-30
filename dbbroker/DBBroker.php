63ce28c9f693eb415532562f074c757eae7f96b5
Dusko Drljaca <dusko.drljaca@consultech.rs>
2018-08-30 23:45:16 +0200

    On branch master

---
+++ dbbroker/DBBroker.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DBBroker
+ *
+ * @author drlja
+ */
+class DBBroker {
+    //put your code here
+}
